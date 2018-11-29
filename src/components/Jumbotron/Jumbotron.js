2d818f210a227015c79d6448c1577ec6bee11743
miramira <cmera001@fiu.edu>
2018-11-29 12:29:14 -0500

    Updated styling to jumbotron background image and text.

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import { Container, Row, Col } from 'reactstrap';
 import style from './Jumbotron.module.css';
 
 const jumbotron = ( props ) => {
   return (
     <>
-      <header className={style.Header}>
-        <h1 className={style.HeaderText}>
-          My Skills Tailored <br />
-          Resume Builder
-        </h1>
-      </header>
+    <Container className={style.Box}>
+      <Row>
+        <Col xs='12' md='3' className={style.Box2}>
+          <h1 className={style.HeaderTextStyle}>
+            My <span className={style.HeaderTextStyle2}>Skills</span>
+          </h1>
+        </Col>
+        <Col xs='12' md='9' className={style.Box3}>
+          <header className={style.Header}></header>
+        </Col>
+      </Row>
+    </Container>
     </>
   )
 };
